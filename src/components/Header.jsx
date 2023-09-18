9fbeb93b0412528bb1d7c64fc61288d4450f1e7c
Quentin Ribardière quentin.rbd@gmail.com <quentin.rbd@gmail.com>
2023-09-18 18:32:03 +0200

    debut header

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <span>Quentin Ribardière</span>
+        <nav>
+          <ul>
+            <a href="#">Home</a>
+            <a href="#">Qui suis-je ?</a>
+            <a href="#">Projets</a>
+            <a href="#">Contact</a>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
