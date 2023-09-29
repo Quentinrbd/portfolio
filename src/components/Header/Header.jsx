ab81ad82005ab05cd5626560f648c5899a1f1496
Quentin Ribardière quentin.rbd@gmail.com <quentin.rbd@gmail.com>
2023-09-29 14:51:04 +0200

    responsive header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,16 @@ import {Link} from "react-scroll"
 export default function Header() {
   return (
     <header className="header">
-      <span>Quentin Ribardière</span>
       <nav>
-        <ul>
-        <Link activeClass="active" to="home" spy={true} smooth={true} offset={-100} duration={600}>Home</Link>
-        <Link activeClass="active" to="bio" spy={true} smooth={true} offset={-400} duration={600}>Qui suis-je ?</Link>
-        <Link activeClass="active" to="projet" spy={true} smooth={true} offset={-115} duration={600}>Projets</Link>
-        <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-100} duration={600}>Contact</Link>
-        </ul>
+        <div className="l-side">
+          <span>Quentin Ribardière</span>
+        </div>
+        <div className="r-side">
+          <Link activeClass="active" to="home" spy={true} smooth={true} offset={-100} duration={600}>Home</Link>
+          <Link activeClass="active" to="bio" spy={true} smooth={true} offset={-400} duration={600}>Qui suis-je ?</Link>
+          <Link activeClass="active" to="projet" spy={true} smooth={true} offset={-115} duration={600}>Projets</Link>
+          <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-100} duration={600}>Contact</Link>
+        </div>
       </nav>
     </header>
   )
