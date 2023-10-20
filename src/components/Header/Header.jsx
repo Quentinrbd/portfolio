8252e20f8cc1af704273db826e2c3fe44992c58d
Quentin Ribardière quentin.rbd@gmail.com <quentin.rbd@gmail.com>
2023-10-20 15:05:37 +0200

    last fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
         </div>
         <div className="r-side">
           <Link activeClass="active" to="home" spy={true} smooth={true} offset={-100} duration={600}>Home</Link>
-          <Link activeClass="active" to="bio" spy={true} smooth={true} offset={-400} duration={600}>Qui suis-je ?</Link>
+          <Link activeClass="active" to="bio" spy={true} smooth={true} offset={-300} duration={600}>Qui suis-je ?</Link>
           <Link activeClass="active" to="projet" spy={true} smooth={true} offset={-115} duration={600}>Projets</Link>
           <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-100} duration={600}>Contact</Link>
         </div>
